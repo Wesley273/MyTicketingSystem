753552c6a92b0759db44d0f9839cf39a13d5a6ca
Wesley <68091994+Wesley273@users.noreply.github.com>
2022-11-23 15:33:39 +0800

    update

--- src/ticketingsystem/CoachSection.java
+++ src/ticketingsystem/CoachSection.java
@@ -5,32 +5,32 @@ import java.util.concurrent.ThreadLocalRandom;
 
 class CoachSection {
 
-	private final int coachId;//车厢序号
-	private final int seatNum;//座位数目
-	private ArrayList<SeatSection> seatList;//座位列表
+	private final int coachId;// 车厢序号
+	private final int seatNum;// 座位数目
+	private ArrayList<SeatSection> seatList;// 座位列表
 
 	public CoachSection(final int coachId, final int seatNum) {
 
 		this.coachId = coachId;
 		this.seatNum = seatNum;
-		//建立数组，存储座位的情况
+		// 建立数组，存储座位的情况
 		seatList = new ArrayList<SeatSection>(seatNum);
 
 		int seatId = 1;
-		while(seatId <= seatNum){
+		while (seatId <= seatNum) {
 			this.seatList.add(new SeatSection(seatId));
-		    seatId++;
+			seatId++;
 		}
 	}
 
-	//尝试买票
+	// 尝试买票
 	public Ticket initSeal(final int departure, final int arrival) {
 
 		Ticket ticket = new Ticket();
 		int randSeat = ThreadLocalRandom.current().nextInt(this.seatNum);
 
 		int i = 0;
-		while(i < this.seatNum){
+		while (i < this.seatNum) {
 
 			int resultSeatId = this.seatList.get(randSeat).initSeal(
 					departure, arrival);
@@ -46,12 +46,12 @@ class CoachSection {
 		return null;
 	}
 
-	//尝试查询
+	// 尝试查询
 	public int initInquiry(final int departure, final int arrival) {
 
 		int ticSum = 0;
 		int i = 0;
-		while(i < this.seatNum){
+		while (i < this.seatNum) {
 			ticSum += this.seatList.get(i).initInquiry(departure, arrival);
 			i++;
 		}
@@ -59,7 +59,7 @@ class CoachSection {
 
 	}
 
-	//尝试退票
+	// 尝试退票
 	public boolean initRefund(final int seatId, final int departure,
 			final int arrival) {
 		return this.seatList.get(seatId - 1).initRefund(departure, arrival);
