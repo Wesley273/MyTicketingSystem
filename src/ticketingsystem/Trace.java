f0cf0d3643e74030a3a817c75184166c50b903af
Wesley <68091994+Wesley273@users.noreply.github.com>
2022-11-23 15:30:14 +0800

    Update Trace.java

--- src/ticketingsystem/Trace.java
+++ src/ticketingsystem/Trace.java
@@ -2,26 +2,6 @@ package ticketingsystem;
 
 import java.util.*;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-class ThreadId {
-	// Atomic integer containing the next thread ID to be assigned
-	private static final AtomicInteger nextId = new AtomicInteger(0);
-
-	// Thread local variable containing each thread's ID
-	private static final ThreadLocal<Integer> threadId = new ThreadLocal<Integer>() {
-		@Override
-		protected Integer initialValue() {
-			return nextId.getAndIncrement();
-		}
-	};
-
-	// Returns the current thread's unique ID, assigning it if necessary
-	public static int get() {
-		return threadId.get();
-	}
-}
-
 public class Trace {
 	final static int threadnum = 4;
 	final static int routenum = 3; // route is designed from 1 to 3
